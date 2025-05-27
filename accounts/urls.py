8dee166ade9c3ce67a14ebc7d71b9cd1cac2fbcd
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2025-05-27 09:39:02 +0200

    create new app accounts

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name='accounts'
+
+
+urlpatterns = [
+    path('signup',views.signup,name='signup'),
+    path('activate_code',views.actiavte_code,name='activate-code'),
+
+    
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def signup(request):
+    return render(request,'accounts/signup.html',{})
+
+
+def actiavte_code(request):
+    return render(request,'accounts/activate_code.html',{})
